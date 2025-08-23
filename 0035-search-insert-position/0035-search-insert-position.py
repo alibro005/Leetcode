85a02bc8128aabf7f3247d128340947072d2f3cd
Muhammad Ali Siddiqui <itsalibro8@gmail.com>
2025-08-23 11:59:02 +0500

    Time: 0 ms (100%), Space: 18.2 MB (93.91%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,19 +1,30 @@
 class Solution:
-    def lowerBound(self,nums,target):
-        n = len(nums)
-        left , right = 0 , n - 1
+    # def lowerBound(self,nums,target):
+    #     n = len(nums)
+    #     left , right = 0 , n - 1
 
-        ans = n
+    #     ans = n
 
-        while left <= right:
-            mid = left + (right-left)//2
+    #     while left <= right:
+    #         mid = left + (right-left)//2
 
-            if nums[mid] >= target:
-                ans = mid
-                right = mid - 1
-            else:
-                left = mid + 1
-        return ans
+    #         if nums[mid] >= target:
+    #             ans = mid
+    #             right = mid - 1
+    #         else:
+    #             left = mid + 1
+    #     return ans
 
     def searchInsert(self, nums: List[int], target: int) -> int:
-        return self.lowerBound(nums,target)
+        # return self.lowerBound(nums,target)
+        
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
