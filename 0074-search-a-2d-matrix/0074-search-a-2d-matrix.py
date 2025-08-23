5f9cdf5c242bc2f8e20f5bd4724d603e9573db56
Muhammad Ali Siddiqui <itsalibro8@gmail.com>
2025-08-23 17:57:21 +0500

    Time: 1 ms (6.35%), Space: 18.2 MB (72.09%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,9 +3,24 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
 
-        for i in range(rows):
-            for j in range(cols):
-                if matrix[i][j] == target:
-                    return True
-        
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if matrix[i][j] == target:
+        #             return True
+
+        l = 0
+        r = (rows * cols) - 1
+
+        while l <= r:
+
+            mid = l + (r - l) // 2
+            mid_val = matrix[mid // cols][mid % cols]
+
+            if mid_val == target:
+                return True
+            elif mid_val < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
         return False
