0c2ddf8469a8ffe627e8a0c865646d9a8dd94201
Muhammad Ali Siddiqui <itsalibro8@gmail.com>
2025-09-01 15:49:57 +0500

    Time: 92 ms (21.34%), Space: 27.7 MB (68.9%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -4,34 +4,58 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
-    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        temp1 = headA
-        temp2 = headB
+    def getIntersectionNode( self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
 
-        l1 =  0
-        l2 = 0
+        p1 = headA
+        p2 = headB
 
-        while temp1 is not None:
-            temp1 = temp1.next
-            l1 += 1
+        count = 0
 
-        while temp2 is not None:
-            temp2 = temp2.next
-            l2 += 1
+        while True:
+            if p1 == p2:
+                return p1
 
-        slow = headA
-        fast = headB
+            p1 = p1.next if p1 else None
+            p2 = p2.next if p2 else None
 
-        if l1 > l2:
-            for _ in range(l1 - l2):
-                slow = slow.next
-        else:
-            for _ in range(l2 - l1):
-                fast = fast.next
 
-        while slow != fast:
-                slow = slow.next
-                fast = fast.next
-            
-        return slow
+            if p1 == None:
+                p1 = headB
+                count += 1
+            if p2 == None:
+                p2 = headA
+
+            if count > 1:
+                return None
+
+        # temp1 = headA
+        # temp2 = headB
+
+        # l1 =  0
+        # l2 = 0
+
+        # while temp1 is not None:
+        #     temp1 = temp1.next
+        #     l1 += 1
+
+        # while temp2 is not None:
+        #     temp2 = temp2.next
+        #     l2 += 1
+
+        # slow = headA
+        # fast = headB
+
+        # if l1 > l2:
+        #     for _ in range(l1 - l2):
+        #         slow = slow.next
+        # else:
+        #     for _ in range(l2 - l1):
+        #         fast = fast.next
+
+        # while slow != fast:
+        #         slow = slow.next
+        #         fast = fast.next
+
+        # return slow
