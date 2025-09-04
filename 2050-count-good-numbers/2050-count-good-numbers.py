3e23b5b590fed4e215faafbb71c70f1b1505b6d7
Muhammad Ali Siddiqui <itsalibro8@gmail.com>
2025-09-04 11:50:22 +0500

    Time: 0 ms (100%), Space: 18 MB (24.89%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.py
+++ 2050-count-good-numbers/2050-count-good-numbers.py
@@ -1,8 +1,20 @@
 class Solution:
+    def power(self, x, n, mod):
+        if n == 0:
+            return 1
+
+        half = self.power(x, n // 2, mod)
+        result = (half * half) % mod
+
+        if n % 2 == 1:
+            result = (result * x) % mod
+
+        return result
+
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10**9 + 7
 
         even_count = (n + 1) // 2
         odd_count = n // 2
 
-        return (pow(5, even_count, MOD)) * (pow(4, odd_count, MOD)) % MOD
+        return (self.power(5, even_count, MOD)) * (self.power(4, odd_count, MOD)) % MOD
