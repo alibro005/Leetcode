5e20063dca23f18cfab996c5e3e606382f261ffc
Muhammad Ali Siddiqui <itsalibro8@gmail.com>
2025-09-13 12:48:33 +0500

    Time: 11 ms (49.17%), Space: 18.2 MB (96.49%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -12,6 +12,6 @@ class Solution:
             min_so_far = min(nums[i], min_so_far * nums[i])
             max_so_far = max(nums[i], max_so_far * nums[i])
 
-            maxProduct = max(maxProduct, max_so_far)
-
+            maxProduct = max(maxProduct , max_so_far)
+        
         return maxProduct
